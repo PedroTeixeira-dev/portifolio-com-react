9983b2626f5b97d5ca119925d71267f57b95e020
Pedro Teixeira <pedro15at@gmail.com>
2023-12-27 15:33:10 -0300

    "criando o esqueleto do projeto"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/PedroTeixeira-dev.png" alt="" />
+    <Titulo>Pedro Teixeira</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam debitis
+      sapiente, corrupti libero voluptatem suscipit omnis excepturi accusantium
+      ipsa ut, sequi eum quod saepe cum voluptates qui quos odit cumque?
+    </p>
+  </section>
+)
+
+export default Sobre
