6d0252f32f5599426648158ff1766a35694815c9
Pedro Teixeira <pedro15at@gmail.com>
2024-01-02 10:13:51 -0300

    "construindo a estrutura e a secao sobre"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       accusantium ipsa ut, sequi eum quod saepe cum voluptates qui quos odit
       cumque?
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=PedroTeixeira-dev&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=PedroTeixeira-dev&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
