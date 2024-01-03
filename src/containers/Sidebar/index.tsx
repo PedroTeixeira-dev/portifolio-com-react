6a924afb75152b1406255ba5964732de1381f66c
Pedro Teixeira <pedro15at@gmail.com>
2024-01-03 13:48:47 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/PedroTeixeira-dev.png" alt="" />
-    <Titulo fontSize={20}>Pedro Teixeira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Pedro Teixeira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        PedroTeixeira-Dev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
